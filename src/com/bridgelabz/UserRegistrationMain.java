453de0caef6cb12de9bfa9b3f45b6f8a91cb79d8
Shahid-sk <mail2sk.shahid@gmail.com>
2021-08-26 16:35:45 +0530

    added: created a method to add special character

--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -4,7 +4,7 @@ public class UserRegistrationMain {
 
 	public static void main(String[] args) {
 		UserRegistrationImpl userRegistration = new UserRegistrationImpl();
-		ValidateSampleEmails.email();
+		userRegistration.validateUserDetails();
 
 	}
 }
